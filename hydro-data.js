346a44e08337d70f044cc2b252b22f6bdeb5755e
Travis <bloomfield.travis@gmail.com>
2016-03-27 15:25:03 +1100

    Add commander to read command line arguments.

--- hydro-data.js
+++ hydro-data.js
@@ -1,12 +1,25 @@
 var fs = require('fs');
 var configLoader = require('./modules/config-loader');
+var program = require('commander');
 var parseExcel = require('./modules/parse-excel');
 var MongoDBStorage = require('./modules/mongodb-storage');
 MongoDBStorage.connect('mongodb://localhost:27017/hydro-data');
 
-// Check minimum number of arguments
-if (process.argv.length < 2) {
-  console.log("Incorrect number of arguments. Usage: hydro-data <filename>");
+program
+  .version('0.0.1')
+  .description('Parse Hydro Tasmania system storage data spreadsheet.')
+  .option('-i, --in-file [filename]', 'Source data file.')
+  .option('-o, --out-file [filename]', 'Write parsed data to a file.')
+  .option('-m, --mongodb [url]', 'Store parsed data in MongoDB database.')
+  .parse(process.argv);
+
+if (!program.inFile) {
+  console.log('Source data file required.');
+  process.exit(1);
+}
+
+if (!(program.outFile || program.mongodb)) {
+  console.log('Must specify either an output file or MongoDB connection.');
   process.exit(1);
 }
 
@@ -18,24 +31,20 @@ try {
   process.exit(1);
 }
 
-// Get file to load from command line
-var fileName = process.argv[2];
-
 // Check we can read the file. If not error and exit.
 try {
-  fs.accessSync(fileName, fs.R_OK, function(err) {
-    console.log('Cannot read file' + fileName);
+  fs.accessSync(program.inFile, fs.R_OK, function(err) {
+    console.log('Cannot read file' + program.inFile);
     process.exit(1);
   });
 } catch (err) {
-  console.log('File ' + fileName + ' not found.');
+  console.log('File ' + program.inFile + ' not found.');
   console.log(err);
   process.exit(1);
 }
 
 // Parse file
-var lakeLevels = parseExcel(fileName, config);
-
+var lakeLevels = parseExcel(program.inFile, config);
 
 var complete = function() {
   MongoDBStorage.close();
